cddd35d882aab62db71374e96409c208bd64d6bc
minhlucvan <toilatoi123>
2017-02-12 11:34:05 +0700

    setup project starter

---
+++ gulpfile.babel.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var babel = require('gulp-babel');
+var mocha = require('gulp-mocha');
+var rename = require('gulp-rename');
+
+gulp.task('test', function(){ 
+    gulp.src('./test/index.js')
+        .pipe(mocha())
+        .once('error', function( err ){
+            console.error(err);
+        });
+});
+
+gulp.task('lint', function(){
+    return gulp.src(['**/*.js','!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+ 
+gulp.task('build', function(){
+    return gulp.src('./src/index.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(rename("react-attr.js"))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['test', 'lint', 'build'], function () {
+    console.log("DONE!, everything is perfect.");
+});
